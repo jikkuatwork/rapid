a66e437a4849951efcf4110c35d4483f23940088
Jikku Jose <jikku@storybrain.com>
2024-05-28 19:35:59 +0530

    Purge unwanted components

--- petite-vue/app.js
+++ petite-vue/app.js
@@ -1,9 +1,3 @@
 import { createApp } from "https://cdn.jsdelivr.net/npm/petite-vue@0.4.1/+esm"
 
-import { store } from "./store.js"
-import { Avatar } from "./components/Avatar/index.js"
-
-createApp({
-  Avatar,
-  store,
-}).mount()
+createApp({}).mount()
